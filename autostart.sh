dd969bfe8168bf541fac1791c828a3ebef22ac1d
eitan-small <18101612160@163.com>
2023-02-06 23:25:35 +0800

    编写 autostart 脚本

---
+++ autostart.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# DWM自启动脚本
+
+settings() {
+    [ $1 ] && sleep $1
+}
+
+daemons() {
+    [ $1 ] && sleep $1
+    fcitx5 &                                  # 开启输入法
+    flameshot &                               # 截图要跑一个程序在后台 不然无法将截图保存到剪贴板
+    dunst -conf ~/scripts/config/dunstrc & # 开启通知server
+    picom --config ~/scripts/config/picom.conf >> /dev/null 2>&1 & # 开启picom
+}
+
+cron() {
+    [ $1 ] && sleep $1
+    let i=0
+    while true; do
+        [ $((i % 300)) -eq 0 ] && feh --randomize --bg-fill ~/Pictures/wallpaper/* # 每300秒更新壁纸
+        sleep 10; let i+=10
+    done
+}
+
+settings 1 &                                  # 初始化设置项
+daemons 3 &                                   # 后台程序项
+cron &                                      # 定时任务项
